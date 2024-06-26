89d9705b019fe7e2fd4675b61fc8c588a3c49a49
PaulaR1998 <paula.rafaela.castro@hotmail.com>
2024-06-26 17:13:11 -0300

    atualização

--- cypress/support/pagesObject/auth.js
+++ cypress/support/pagesObject/auth.js
@@ -1,9 +1,11 @@
 Cypress.Commands.add('auth', () => {
     cy.visit('https://tradeappone.com.br/login');
 
-    cy.get('#usurio-1').type('46918303830');
-    cy.get('#senha-1').type('Amoalex100%');
+    cy.get('#usurio-1').type('05709976741');
+    cy.get('#senha-1').type('Trade@2024');
     cy.get('.login-field > .mt-2').invoke('removeAttr', 'disabled');
 
     cy.get('.login-field > .mt-2').click({ force: true });
+
+
 });
